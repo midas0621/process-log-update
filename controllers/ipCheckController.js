72061086a0574b46c7c4ad3df5671131cf775638
ezain501 <ezain501@gmail.com>
2025-04-30 11:32:14 -0300

    modify connction

--- controllers/ipCheckController.js
+++ controllers/ipCheckController.js
@@ -5,17 +5,30 @@ require('dotenv').config(); // Ensure dotenv is required to load environment var
 
 const uri = `mongodb+srv://filipporter9017:${process.env.DB_PASSWORD}@cluster0.7nw96kp.mongodb.net/gd/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const client = new MongoClient(uri, {socketTimeoutMS: 45000} );
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  socketTimeoutMS: 45000,
+  tlsAllowInvalidCertificates: false, // set to true only if testing with self-signed certs
+});
 
 async function insertQuery({query}) {
   try {
     await client.connect();
+    console.log("client is connected")
     const database = client.db("gd");
+    console.log("db is connected")
     const table = database.collection("vercel_upload");
+    console.log("table is connected")
     const result = await table.insertOne({query});
+
+  } catch (err) {
+    console.log("error is occurred")
+    await client.close();
   } finally {
     await client.close();
-  }
+  } 
 }
 
 exports.getIpCheck = asyncErrorHandler(async (req, res, next) => {
